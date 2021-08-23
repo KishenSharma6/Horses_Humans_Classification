b1da7c44b29d2b0ee9f51d9f79b2e798e3a41581
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-23 14:11:09 -0700

    Added preview_images

---
+++ src/load_data.py
@@ -0,0 +1,26 @@
+import os
+import random
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+def preview_images(image_count, path, figsize = (20,20)):
+    """Publishes matplotlib visualization showing randomly selected images from folder set by path variable.
+
+    Args:
+        image_count (int): number of images you would like randomly chosen for visualization
+        path (string): string path to folder containing images for visualization on local machine
+        figsize (tuple): tuple contain plot dimensions for matplotlib plot
+    """
+    plt.figure(figsize=figsize)
+
+    target_data= path
+
+    for i in range(image_count):
+        file= random.choice(os.listdir(target_data))
+        image_path= os.path.join(target_data, file)
+        img= mpimg.imread(image_path)
+        ax= plt.subplot(1,image_count, i+1)
+        ax.title.set_text(file)
+        plt.axis('off')
+        plt.imshow(img)
