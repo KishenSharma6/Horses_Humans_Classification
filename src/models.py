00e982230c0617e1037f2331ca7cbcfa89bfdf60
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-02-16 13:07:01 -0800

    Added Test_loop

--- src/models.py
+++ src/models.py
@@ -46,5 +46,20 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         loss, current = loss.item(), batch * len(X)
         print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]") 
 
-def 
+def test_loop(dataloader, model, loss_fn):
+    """Deploys trained Pytorch model onto test data for evaluation using loss_fn
+    """
+    size= len(dataloader.dataset)
+    num_batches= len(dataloader)
+    test_loss, correct = 0, 0
 
+    with torch.no_grad():
+        for (X, y) in dataloader:
+            preds= model(X.float())
+
+            test_loss += loss_fn(preds, y.flatten()).item()
+            correct += (preds.argmax(1) == y.flatten()).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
